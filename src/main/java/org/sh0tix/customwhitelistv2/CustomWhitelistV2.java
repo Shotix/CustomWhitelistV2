469d1c26af1dcf4d538549d48d499f9451431ad7
Shotix <tregnet04@gmail.com>
2024-02-15 17:13:05 +0100

    - Added the msgmoderator command

--- src/main/java/org/sh0tix/customwhitelistv2/CustomWhitelistV2.java
+++ src/main/java/org/sh0tix/customwhitelistv2/CustomWhitelistV2.java
@@ -1,8 +1,13 @@
 package org.sh0tix.customwhitelistv2;
 
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.model.group.Group;
+import net.luckperms.api.node.types.PermissionNode;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.sh0tix.customwhitelistv2.commands.CustomWhitelistV2Commands;
 import org.sh0tix.customwhitelistv2.commands.LoginCommand;
+import org.sh0tix.customwhitelistv2.commands.MsgModeratorCommand;
 import org.sh0tix.customwhitelistv2.listener.ChatListener;
 import org.sh0tix.customwhitelistv2.listener.EventListener;
 
@@ -20,12 +25,13 @@ public final class CustomWhitelistV2 extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         instance = this;
-
+        
         // Register the events
         getServer().getPluginManager().registerEvents(new EventListener(), this);
         getServer().getPluginManager().registerEvents(new ChatListener(), this);
         
         // Register the commands
+        Objects.requireNonNull(getCommand("msgModerator")).setExecutor(new MsgModeratorCommand());
         Objects.requireNonNull(getCommand("login")).setExecutor(new LoginCommand());
         Objects.requireNonNull(getCommand("customWhitelistV2")).setTabCompleter(new CustomWhitelistV2TabCompleter());
         Objects.requireNonNull(getCommand("customWhitelistV2")).setExecutor(new CustomWhitelistV2Commands());
