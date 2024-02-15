d69b68d28c76c657a9a57bace3cee35958943900
Shotix <tregnet04@gmail.com>
2024-02-15 17:10:34 +0100

    - Added the logic to msg moderators

---
+++ src/main/java/org/sh0tix/customwhitelistv2/commands/MsgModeratorCommand.java
@@ -0,0 +1,32 @@
+package org.sh0tix.customwhitelistv2.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class MsgModeratorCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length == 0) {
+            sender.sendMessage("Please provide a message");
+            return true;
+        }
+
+        String message = String.join(" ", args);
+        
+        // Add the following präfix to the message: "[Message to the moderators] " in the color blue. The rest of the message should be in the color white
+        message = "§a[Message to the moderators] §f" + message;
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (player.hasPermission("customwhitelistv2.manage")) {
+                player.sendMessage(message);
+            }
+        }
+
+        return true;
+    }
+}
