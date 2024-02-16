d926e464c369c6cb1d65183878da34daaa3d2bd6
Shotix <tregnet04@gmail.com>
2024-02-16 13:31:57 +0100

    - Added a check if PaperMC is used as the sever jar  - Added a check if LuckPerms is installed  - Create all necessary LuckPerms permissions

--- src/main/java/org/sh0tix/customwhitelistv2/CustomWhitelistV2.java
+++ src/main/java/org/sh0tix/customwhitelistv2/CustomWhitelistV2.java
@@ -1,9 +1,13 @@
 package org.sh0tix.customwhitelistv2;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.group.Group;
 import net.luckperms.api.node.types.PermissionNode;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.sh0tix.customwhitelistv2.commands.CustomWhitelistV2Commands;
 import org.sh0tix.customwhitelistv2.commands.DebugCommand;
@@ -26,6 +30,16 @@ public final class CustomWhitelistV2 extends JavaPlugin {
     
     @Override
     public void onEnable() {
+        // Check if PaperMC is used 
+        if (!getServer().getVersion().contains("Paper")) {
+            getLogger().severe("This plugin is designed to work with PaperMC only! Disabling CustomWhitelistV2...");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        
+        // Check if the LuckPerms plugin is installed and enabled
+        if (checkForLuckPermsSetup()) return;
+
         // Plugin startup logic
         instance = this;
         
@@ -42,8 +56,54 @@ public final class CustomWhitelistV2 extends JavaPlugin {
         Objects.requireNonNull(getCommand("customWhitelistV2")).setExecutor(new CustomWhitelistV2Commands());
     }
 
+    private boolean checkForLuckPermsSetup() {
+        // Check if the LuckPerms plugin is installed
+        if (getServer().getPluginManager().getPlugin("LuckPerms") == null) {
+            getLogger().severe("LuckPerms plugin not found! Disabling CustomWhitelistV2...");
+            getServer().getPluginManager().disablePlugin(this);
+            return true;
+        }
+
+        // Check if the LuckPerms plugin is enabled
+        if (!Objects.requireNonNull(getServer().getPluginManager().getPlugin("LuckPerms")).isEnabled()) {
+            getLogger().severe("LuckPerms plugin is not enabled! Disabling CustomWhitelistV2...");
+            getServer().getPluginManager().disablePlugin(this);
+            return true;
+        }
+
+        // Check if the necessary permissions are existing and add them if not
+        LuckPerms api = LuckPermsProvider.get();
+        Group group = api.getGroupManager().getGroup("default");
+        if (group == null) {
+            getLogger().severe("The group 'default' does not exist! Disabling CustomWhitelistV2...");
+            getServer().getPluginManager().disablePlugin(this);
+            return true;
+        }
+
+        // Create the permissions
+        PermissionNode.builder("customwhitelistv2.manage").build();
+        PermissionNode.builder("customwhitelistv2.administrator").build();
+        PermissionNode.builder("customwhitelistv2.login").build();
+        return false;
+    }
+    
+
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        // Cancel all tasks associated with this plugin
+        getServer().getScheduler().cancelTasks(this);
+
+        // Send a message to the console
+        getServer().getConsoleSender().sendMessage("CustomWhitelistV2 plugin has been disabled!");
+
+        // Send a message to all administrators
+        for (Player player : getServer().getOnlinePlayers()) {
+            if (player.hasPermission("customwhitelistv2.administrator")) {
+                player.sendMessage(Component.text().append(Component.text("CustomWhitelistV2 plugin has been disabled!", NamedTextColor.RED, TextDecoration.BOLD)));
+            }
+        }
+
+        // Nullify any static instances to help with garbage collection
+        instance = null;
     }
 }
