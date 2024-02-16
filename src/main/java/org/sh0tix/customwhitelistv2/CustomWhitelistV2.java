b76b6165565ed8305a81fb3352fb11b1971f200a
Shotix <tregnet04@gmail.com>
2024-02-16 12:16:37 +0100

    - Added a debug var and the debugging tab completer

--- src/main/java/org/sh0tix/customwhitelistv2/CustomWhitelistV2.java
+++ src/main/java/org/sh0tix/customwhitelistv2/CustomWhitelistV2.java
@@ -6,6 +6,7 @@ import net.luckperms.api.model.group.Group;
 import net.luckperms.api.node.types.PermissionNode;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.sh0tix.customwhitelistv2.commands.CustomWhitelistV2Commands;
+import org.sh0tix.customwhitelistv2.commands.DebugCommand;
 import org.sh0tix.customwhitelistv2.commands.LoginCommand;
 import org.sh0tix.customwhitelistv2.commands.MsgModeratorCommand;
 import org.sh0tix.customwhitelistv2.listener.ChatListener;
@@ -14,6 +15,8 @@ import org.sh0tix.customwhitelistv2.listener.EventListener;
 import java.util.Objects;
 
 public final class CustomWhitelistV2 extends JavaPlugin {
+    
+    public static boolean debugMode = false;
     
     private static CustomWhitelistV2 instance;
     
@@ -31,6 +34,8 @@ public final class CustomWhitelistV2 extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new ChatListener(), this);
         
         // Register the commands
+        Objects.requireNonNull(getCommand("customWhitelistV2Debugging")).setExecutor(new DebugCommand());
+        Objects.requireNonNull(getCommand("customWhitelistV2Debugging")).setTabCompleter(new CustomWhitelistV2DebuggingTabCompleter());
         Objects.requireNonNull(getCommand("msgModerator")).setExecutor(new MsgModeratorCommand());
         Objects.requireNonNull(getCommand("login")).setExecutor(new LoginCommand());
         Objects.requireNonNull(getCommand("customWhitelistV2")).setTabCompleter(new CustomWhitelistV2TabCompleter());
