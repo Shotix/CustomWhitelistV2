5ffddbcde11f31d0ec869add532892ca16ff6610
Shotix <72971195+Shotix@users.noreply.github.com>
2024-03-26 13:33:15 +0100

    - Updated gitignore

--- src/main/java/org/sh0tix/customwhitelistv2/web/WebServer.java
+++ src/main/java/org/sh0tix/customwhitelistv2/web/WebServer.java
@@ -4,17 +4,19 @@ import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.bukkit.Bukkit;
+import org.sh0tix.customwhitelistv2.handlers.PasswordHandler;
 import org.sh0tix.customwhitelistv2.handlers.PlayerStatusHandler;
 import org.sh0tix.customwhitelistv2.whitelist.CWV2Player;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class WebServer {
     private HttpServer server;
@@ -25,6 +27,7 @@ public class WebServer {
             server = HttpServer.create(new InetSocketAddress(7777), 0);
             server.createContext("/", new MyHandler());
             server.createContext("/api/players", new PlayerHandler());
+            server.createContext("/api/updatePassword", new UpdatePasswordHandler());
             server.setExecutor(null); // creates a default executor
             server.start();
         } catch (Exception e) {
@@ -38,6 +41,57 @@ public class WebServer {
             server.stop(0);
         }
     }
+
+    static class UpdatePasswordHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            // Read the request body
+            InputStreamReader isr = new InputStreamReader(t.getRequestBody(), StandardCharsets.UTF_8);
+            BufferedReader br = new BufferedReader(isr);
+            String query = br.readLine();
+
+            // Parse the current and new password from the request body
+            Map<String, String> parameters = parseFormData(query);
+            String currentPassword = parameters.get("currentPassword");
+            String newPassword = parameters.get("newPassword");
+
+            // Check the current password
+            boolean isCorrect = PasswordHandler.checkPassword(currentPassword);
+
+            // If the current password is correct, update the password
+            if (isCorrect) {
+                PasswordHandler.updatePassword(newPassword);
+            }
+
+            // Prepare the response
+            String response = new Gson().toJson(isCorrect);
+            byte[] bytes = response.getBytes(StandardCharsets.UTF_8);
+
+            // Send the response
+            t.sendResponseHeaders(200, bytes.length);
+            OutputStream os = t.getResponseBody();
+            os.write(bytes);
+            os.close();
+        }
+
+        private Map<String, String> parseFormData(String formData) {
+            Map<String, String> parameters = new HashMap<>();
+            String[] pairs = formData.split("&");
+            for (String pair : pairs) {
+                String[] keyValue = pair.split("=");
+                if (keyValue.length > 1) {
+                    String key = keyValue[0];
+                    String value = keyValue[1];
+                    parameters.put(key, value);
+                } else {
+                    parameters.put(keyValue[0], "");
+                }
+            }
+            
+            return parameters;
+        }
+    }
+    
 
     static class PlayerHandler implements HttpHandler {
         @Override
